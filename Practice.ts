e780645b44a1ebe92ab0d0d8555c2550a370ab81
MuhammadLuqmanSawati <143633750+MuhammadLuqmanSawati@users.noreply.github.com>
2024-03-22 16:25:31 +0500

    Add files via upload

---
+++ Practice.ts
@@ -0,0 +1,90 @@
+// let favoriteColor = "red";
+// console.log(favoriteColor)
+// favoriteColor = " Green" ;
+// console.log (favoriteColor)
+
+
+// let username : string = "Luqman" ;
+
+// username = "Khan" 
+
+// // console.log ("Hellow World");
+
+// 9 Mar 2024 
+
+// let numberoffaculity = 4 ;
+// let numerofmanagment= 3;
+// let numberofabsentfaculity = 2;
+
+// let totalnumberoffaculity = numberoffaculity * 21;
+
+// console.log (totalnumberoffaculity);
+
+// let numberofpeople = 20 ;
+// let TotalofBiryani = 84;
+
+// console.log (numberofpeople / TotalofBiryani );
+
+// let firstname = "Luqman"; 
+// let age = 23;
+
+
+// console.log (` Hellow Dear ${firstname} your age is ${age} ?`);
+
+// let index= 10;
+
+// let result= ++index + ++index +6 + --index -2;
+
+// console.log(result);
+
+// let num1 : number= 20;
+// num1 += 4;
+
+// console.log(num1);
+
+// let numb1 = 9;
+// let numb2 = 9;
+
+// console.log (numb1 == numb2);
+
+
+// 16 Mar 2024
+
+// if or else 
+
+let mypercentage = 75;
+
+if (mypercentage >= 90 && mypercentage <100) {
+    console.log ("Your Grade is : A+")
+}
+
+else if (mypercentage >= 80 && mypercentage <90) {
+    console.log ("Your Grade is : A")
+}
+else if (mypercentage >= 70 && mypercentage <80) {
+    console.log ("Your Grade is : B")
+}
+else if (mypercentage >= 60 && mypercentage <70) {
+    console.log ("Your Grade is : C")
+}
+else if (mypercentage >= 50 && mypercentage <60) {
+    console.log ("Your Grade is : D")
+}
+else if (mypercentage >= 40 && mypercentage <50) {
+    console.log ("Your Grade is : E")
+}
+
+else{ console. log ("Faild")}; 
+
+
+
+
+
+
+
+
+
+
+
+
+
